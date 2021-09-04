9f5f55447591e1b8f360df75b4071d663a4c9b63
Kamil Wójcik <wojcikakamil@gmail.com>
2021-09-05 00:46:54 +0200

    2 new tests for create user method

--- src/test/java/com/cloudpi/cloudpi_backend/user/controllers/UserManagementControllerTest.java
+++ src/test/java/com/cloudpi/cloudpi_backend/user/controllers/UserManagementControllerTest.java
@@ -1,33 +1,45 @@
 package com.cloudpi.cloudpi_backend.user.controllers;
 
-import com.cloudpi.cloudpi_backend.security.AccountType;
-import com.cloudpi.cloudpi_backend.user.dto.UserDto;
+import com.cloudpi.cloudpi_backend.configuration.network.LocalNetwork;
+import com.cloudpi.cloudpi_backend.configuration.network.LocalNetworksInfo;
+import com.cloudpi.cloudpi_backend.exepctions.authorization.IllegalIPException;
+import com.cloudpi.cloudpi_backend.exepctions.authorization.PermissionNotFoundException;
+import com.cloudpi.cloudpi_backend.security.CloudPIUser;
+import com.cloudpi.cloudpi_backend.security.CloudPiAuthentication;
+import com.cloudpi.cloudpi_backend.security.permissions.AccountType;
+import com.cloudpi.cloudpi_backend.user.dto.UserDTO;
+import com.cloudpi.cloudpi_backend.user.requests.PostUserRequest;
 import com.cloudpi.cloudpi_backend.user.services.UserRepoService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.security.core.Authentication;
 import org.springframework.test.util.ReflectionTestUtils;
 import java.util.*;
 
+import static com.cloudpi.cloudpi_backend.user.UserTestUtils.createDefaultUser;
+import static com.cloudpi.cloudpi_backend.user.UserTestUtils.createRootUser;
 import static org.mockito.Mockito.doReturn;
 
 
 @ExtendWith(MockitoExtension.class)
 class UserManagementControllerTest {
 
-    private UserRepoService userService = Mockito.mock(UserRepoService.class);
-    private ModelMapper modelMapper = new ModelMapper();
-    private UserManagementController userController = new UserManagementController(userService, modelMapper, "true");
+    private UserRepoService userService;
+    private UserManagementController userController;
 
 
     @BeforeEach
     void setUp() {
         userService = Mockito.mock(UserRepoService.class);
-        modelMapper = new ModelMapper();
-        userController = new UserManagementController(userService, modelMapper, "true");
+        ModelMapper modelMapper = new ModelMapper();
+        LocalNetworksInfo networksInfo = new LocalNetworksInfo(List.of(new LocalNetwork("192.168.0.0", 24)));
+        userController = new UserManagementController(userService, modelMapper, "true", networksInfo);
     }
 
     private void setModificationsOnlyFromLocalNetwork(boolean value) {
@@ -36,13 +48,38 @@ class UserManagementControllerTest {
 
 
     @Test
-    void createNewUser() {
+    void should_throw_Exception_when_user_does_not_have__USER_CREATE__permission() {
         //given
         setModificationsOnlyFromLocalNetwork(false);
-        var userDto = new UserDto(1L, "root", "root@root", "root", "password", false, AccountType.ROOT, new ArrayList());
-        doReturn(Optional.of(userDto)).when(userService).getUser("");
-        //when
-//        userController.createNewUser()
+        var userDto = createDefaultUser();
+        var http = new MockHttpServletRequest();
+        http.setRemoteAddr("192.168.0.101");
+        var auth = new CloudPiAuthentication(new CloudPIUser(userDto));
 
+        doReturn(Optional.of(userDto)).when(userService).getUser("username");
+
+        //then
+        Assertions.assertThrows(PermissionNotFoundException.class, () -> {
+            //when
+            userController.createNewUser(new PostUserRequest(), auth, http);
+        });
     }
+
+    @Test
+    void should_throw_Exception_when_request_not_from_local_network() {
+        //given
+        setModificationsOnlyFromLocalNetwork(true);
+        var userDto = createRootUser();
+        var http = new MockHttpServletRequest();
+        http.setRemoteAddr("109.4.4.105");
+        var auth = new CloudPiAuthentication(new CloudPIUser(userDto));
+
+        //then
+        Assertions.assertThrows(IllegalIPException.class, () -> {
+            //when
+            userController.createNewUser(new PostUserRequest(), auth, http);
+        });
+    }
+
+
 }
