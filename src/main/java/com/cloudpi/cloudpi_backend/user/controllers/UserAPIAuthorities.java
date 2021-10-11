9a8f0098dacf1b75100c3820b961bdd4adc7fbfa
Kamil Wójcik <wojcikakamil@gmail.com>
2021-10-11 22:15:51 +0200

    fixed strange bug

--- src/main/java/com/cloudpi/cloudpi_backend/user/controllers/UserAPIAuthorities.java
+++ src/main/java/com/cloudpi/cloudpi_backend/user/controllers/UserAPIAuthorities.java
@@ -5,16 +5,19 @@ import com.cloudpi.cloudpi_backend.security.authority.annotations.Permission;
 
 @ContainsPermissions
 public class UserAPIAuthorities {
+
+    //TODO add git issue with empty CREATE string
+    //TODO change this strings
     @Permission
     public static final String GET_USERS_DETAILS = "GET_USER_DETAILS";
     @Permission
-    public static final String MODIFY = "";
+    public static final String MODIFY = "1";
     @Permission
-    public static final String CREATE = "";
+    public static final String CREATE = "2";
     @Permission
-    public static final String DELETE = "";
+    public static final String DELETE = "3";
     @Permission
-    public static final String DELETE_SELF = "";
+    public static final String DELETE_SELF = "4";
     @Permission
-    public static final String LOCK = "";
+    public static final String LOCK = "5";
 }
