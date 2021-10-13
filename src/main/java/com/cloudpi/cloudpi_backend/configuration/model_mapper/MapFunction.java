9cbbca41482161d4d05a2fe6c18acf92d92b2610
Kamil Wójcik <wojcikakamil@gmail.com>
2021-10-13 13:16:05 +0200

    removed model mapper

--- src/main/java/com/cloudpi/cloudpi_backend/configuration/model_mapper/MapFunction.java
+++
@@ -1,7 +0,0 @@
-package com.cloudpi.cloudpi_backend.configuration.model_mapper;
-
-import org.modelmapper.ModelMapper;
-
-public interface MapFunction {
-    void addMappingToModelMapper(ModelMapper modelMapper);
-}
